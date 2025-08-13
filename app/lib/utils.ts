24ce34af32be106ed9cbb45943fb0dcebb5c91c1
majdyaq <majd@feelit.tech>
2025-08-13 16:25:17 +0300

    move utils to lib

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,5 +1,12 @@
 import {v4 as uuidv4} from 'uuid';
 
+import {type ClassValue, clsx} from "clsx";
+import {twMerge} from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(inputs))
+}
+
 export function formatSize(bytes: number): string {
     // Guard against invalid input
     if (!isFinite(bytes) || bytes <= 0) {
